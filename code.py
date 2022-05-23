431d77d637310315b2c7d56a42dff7cd4ad2f976
Mike Weiblen <mew@mew.cx>
2022-05-22 19:23:59 -0600

    code.py

--- code.py
+++ code.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+help("modules")
+
+import os
+print("dir(os) : ", dir(os))
+print("os.uname() : ", os.uname())
+
+import board
+print("dir(board) : ", dir(board))
+
+import supervisor
+print("dir(supervisor) : ", dir(supervisor))
+
+import microcontroller
+print("dir(microcontroller) : ", dir(microcontroller))
+print("dir(microcontroller.pin) : ", dir(microcontroller.pin))
+
