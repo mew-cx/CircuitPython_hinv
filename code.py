f9cd4cd0189bc6b46f4d0b2c2711ca8f556971fd
Mike Weiblen <mew@mew.cx>
2022-06-03 21:47:37 -0600

    wip

--- code.py
+++ code.py
@@ -1,17 +1,30 @@
-help("modules")
+# uname/code.py
 
-import os
-#print("\ndir(os) : ", dir(os))
-print("\nos.uname() : ", os.uname())
+print("\nsysinfo ==================")
 
 import board
-print("\ndir(board) : ", dir(board))
-print("\nboard.board_id : ", board.board_id)
+print("\nboard.board_id :", board.board_id)
+print("\ndir(board) :", dir(board))
+
+import os
+#print("\ndir(os) :", dir(os))
+print("\nos.uname() :", os.uname())
 
 import supervisor
-#print("\ndir(supervisor) : ", dir(supervisor))
+#print("\ndir(supervisor) :", dir(supervisor))
 
 import microcontroller
-print("\ndir(microcontroller) : ", dir(microcontroller))
-print("\ndir(microcontroller.pin) : ", dir(microcontroller.pin))
+print("\ndir(microcontroller) :", dir(microcontroller))
+print("\ndir(microcontroller.pin) :", dir(microcontroller.pin))
 
+import sys
+print("\ndir(sys) :", dir(sys))
+
+import micropython
+print("\ndir(micropython) :", dir(micropython))
+
+print("\nhelp('modules') {")
+help('modules')
+print("}")
+
+print("==========================")
