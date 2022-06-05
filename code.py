2bbf77bd42232a9d8041b9a9c9ce031fd0124f2e
Mike Weiblen <mew@mew.cx>
2022-06-04 18:56:43 -0600

    code.py tweaks

--- code.py
+++ code.py
@@ -3,28 +3,32 @@
 print("\nsysinfo ==================")
 
 import board
-print("\nboard.board_id :", board.board_id)
-print("\ndir(board) :", dir(board))
+print("board.board_id :", board.board_id)
+print("dir(board) :", dir(board))
 
 import os
 #print("\ndir(os) :", dir(os))
-print("\nos.uname() :", os.uname())
+print("os.uname() :", os.uname())
 
 import supervisor
 #print("\ndir(supervisor) :", dir(supervisor))
 
 import microcontroller
 print("\ndir(microcontroller) :", dir(microcontroller))
-print("\ndir(microcontroller.pin) :", dir(microcontroller.pin))
+print("dir(microcontroller.pin) :", dir(microcontroller.pin))
 
 import sys
 print("\ndir(sys) :", dir(sys))
+print("sys.version :", sys.version)
+print("sys.platform :", sys.platform)
+print("sys.implementation :", sys.implementation)
+print("sys.path :", sys.path)
+#print("sys.modules :", sys.modules)
 
 import micropython
-print("\ndir(micropython) :", dir(micropython))
+#print("\ndir(micropython) :", dir(micropython))
 
 print("\nhelp('modules') {")
 help('modules')
 print("}")
-
 print("==========================")
