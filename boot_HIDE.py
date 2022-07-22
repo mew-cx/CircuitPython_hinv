928a882207480859e89d12feee50f6567b8b6c10
Mike Weiblen <mew@mew.cx>
2022-07-21 23:47:40 -0600

    boot_HIDE.py : big reorg; create main()

--- boot_HIDE.py
+++ boot_HIDE.py
@@ -1,91 +1,101 @@
-# sysinfo.py
+# sysinfo.py http://mew.cx/  2022-07-22
+# SPDX
 
+import board
+import microcontroller
 import storage
 import os
-import microcontroller
 import sys
+
 import supervisor
-import board
-import errno
 import micropython
+import errno
+
+__version__ = "0.0.0.0"
+__repo__    = "todo"
+
+# functions #################################################################
+
+def HexifyByteArray(data):
+    #return "".join("%02x" % i for i in data)
+    return "".join("{:02x}".format(i) for i in data)
 
 def Filename():
-    #print("uidX", "".join("%02x" % i for i in microcontroller.cpu.uid))
-    #print("uidY", "".join("{:02x}".format(i) for i in microcontroller.cpu.uid))
-    uid = "".join("{:02x}".format(i) for i in microcontroller.cpu.uid)
-    return "{}__{}.txt".format(board.board_id, uid)
+    return "{}__{}.txt".format(
+        board.board_id,
+        HexifyByteArray(microcontroller.cpu.uid))
 
 def PinMap():
     """CircuitPython Essentials Pin Map Script"""
     uc_pins = []
     for ucpin in dir(microcontroller.pin):
         uc_attr = getattr(microcontroller.pin, ucpin)
-        #print(repr(uc_attr))
         if isinstance(uc_attr, microcontroller.Pin):
             b_pins = [ucpin]
             for bpin in dir(board):
                 if getattr(board, bpin) is uc_attr:
                     b_pins.append("board.{}".format(bpin))
-
             if len(b_pins) > 1:
                 uc_pins.append("\t".join(b_pins))
-
-    for i in sorted(uc_pins):
-        print(i)
+    return sorted(uc_pins):
 
 #############################################################################
 
-print("Filename() : ", Filename())
+def main():
+    print("Filename() : ", Filename())
 
-print("board.board_id : ", board.board_id)
-print("sys.implementation : ", sys.implementation)
-print("sys.modules : ", sys.modules)
-print("sys.path : ", sys.path)
-print("sys.platform : ", sys.platform)
-print("sys.version : ", sys.version)
-print("sys.version_info : ", sys.version_info)
-print("os.uname() :", os.uname())
+    print("board.board_id : ", board.board_id)
+    print("uid : ", repr(microcontroller.cpu.uid))
+    #print("uid0 : ", microcontroller.cpus[0].uid)
+    #print("uid1 : ", microcontroller.cpus[1].uid)
 
-#supervisor.set_rgb_status_brightness(100)
+    print("sys.implementation : ", sys.implementation)
+    print("sys.modules : ", sys.modules)
+    print("sys.path : ", sys.path)
+    print("sys.platform : ", sys.platform)
+    print("sys.version : ", sys.version)
+    print("sys.version_info : ", sys.version_info)
+    print("os.uname() :", os.uname())
 
-print("uid : ", repr(microcontroller.cpu.uid))
-#print("uid0 : ", microcontroller.cpus[0].uid)
-#print("uid1 : ", microcontroller.cpus[1].uid)
+    print("os.statvfs(/) : ", os.statvfs("/"))
+    print("os.stat(/code.py) : ", os.stat("/code.py"))
+    print("os.sep : ", os.sep)
 
-print("statvfs / : ", os.statvfs("/"))
-print("stat /code.py : ", os.stat("/code.py"))
-print("sep : ", os.sep)
+    print("\ndir(microcontroller.pin) :", dir(microcontroller.pin))
+
+    print("\ndir(board) :", dir(board))
+    print("\ndir(microcontroller) :", dir(microcontroller))
+    print("\ndir(storage) :", dir(storage))
+    print("\ndir(os) :", dir(os))
+    print("\ndir(sys) :", dir(sys))
+
+    print("\ndir(supervisor) :", dir(supervisor))
+    print("\ndir(micropython) :", dir(micropython))
+    print("\ndir(errno) :", dir(errno))
+
+    print("\nhelp('modules') {")
+    help('modules')
+    print("}")
+
+    print("\nPinMap() {")
+    for i in PinMap():
+        print(i)
+    print("}")
+    print("done =====================")
+
+    os.sync()
+
+# make it so ################################################################
 
 #storage.remount("/", readonly=False) # CPy writable
+main()
 #storage.remount("/", readonly=True)  # CPy readonly
-#print("stat foo", os.stat("/foo.py"))
-#print("getcwd", os.getcwd())
+
+# future ####################################################################
+#microcontroller.reset()
+#print("os.stat foo", os.stat("/foo.py"))
+#print("os.getcwd", os.getcwd())
 #with open("/tmp.txt", "a") as fp:
 #    fp.write("hello, world!")
 #os.rename("/boot.py", "/boot.bak")
-
-#############################################################################
-# uname/code.py
-
-print("\ndir(microcontroller.pin) :", dir(microcontroller.pin))
-
-print("\ndir(board) :", dir(board))
-print("\ndir(os) :", dir(os))
-print("\ndir(sys) :", dir(sys))
-print("\ndir(storage) :", dir(storage))
-print("\ndir(errno) :", dir(errno))
-print("\ndir(micropython) :", dir(micropython))
-print("\ndir(microcontroller) :", dir(microcontroller))
-print("\ndir(supervisor) :", dir(supervisor))
-
-print("\nhelp('modules') {")
-#help('modules')
-print("}")
-
-print("\nPinMap() {")
-PinMap()
-print("}")
-
-os.sync()
-print("done =====================")
-#microcontroller.reset()
+# eof
